69f4a513b5f7de642d499fe393cc1ef5c84a6e90
mazen-khaled <64374092+mazen-khaled@users.noreply.github.com>
2024-09-07 16:55:15 +0300

    Update Os.c

--- AUTOSAR_Project/Os.c
+++ AUTOSAR_Project/Os.c
@@ -6,7 +6,7 @@
  *
  * Description: Source file for Os Scheduler.
  *
- * Author: Mohamed Tarek
+ * Author: Mazen Khaled
  ******************************************************************************/
 
 #include "Os.h"
